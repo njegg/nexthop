e278727b08a18d2497e2c3a9a5836ef0a80be07b
Njegomir <njegomir15@gmail.com>
2022-06-02 02:06:55 +0200

    automatizm

---
+++ .masm_errors.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fr = fopen("MASMLOG.TXT", "r");
+    if (!fr) {
+        printf("Unable to read MASMLOG.TXT: %s\n", strerror(errno));
+        printf("Last compiled version of program will run\n");
+        return 2;
+    }
+
+    char buf[256];
+    char *needle;
+    int errors = -1;
+    while (fgets(buf, 256, fr)) {
+        if (strstr(buf, "Severe  Errors")) {
+            sscanf(buf, "%i", &errors);
+            break;
+        }
+    }
+
+    if (errors == -1) {
+        printf("Unable to check for errors from MASMLOG.TXT");
+        return 1;
+    }
+
+    return errors;
+}
+
---
+++ Makefile
@@ -0,0 +1,24 @@
+run:
+	@dosbox \
+		-c 'mount c "."' \
+	 	-c "c:" \
+		-c "del nexthop.exe;" \
+		-c "cls" \
+		-c "masm.exe /z nexthop; > MASMLOG.TXT" \
+		-c "cls" \
+		-c "type MASMLOG.TXT" \
+		-c "link.exe nexthop.obj; > LINKLOG.TXT" \
+		-c "exit"
+
+	@./masm_errors && \
+		dosbox \
+			-c 'mount c "."' \
+			-c "c:" \
+			-c "nexthop.exe" \
+			-c "del nexthop.obj" \
+			-c "exit"\
+			||\
+		echo;echo "There are errors or warnings:";echo;\
+		cat MASMLOG.TXT
+	
+	
